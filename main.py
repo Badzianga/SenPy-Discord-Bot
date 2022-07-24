35e4cedd3e1d16679e35e0e144f268076d0c6d5d
Tomasz Oszczypko <tomekoszczypko@gmail.com>
2022-07-24 17:26:42 +0200

    Added config file to disable cogs and loggers

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ from dotenv import load_dotenv
 
 
 # Logger -------------------------------------------------------------------- #
-def setup_logger() -> logging.Logger:
+def setup_logger(config: dict) -> logging.Logger:
     """Sets up discord logger and adds streamhandler and filehandler to it."""
     logger = logging.getLogger('discord')
     logger.setLevel(logging.INFO)
@@ -18,20 +18,21 @@ def setup_logger() -> logging.Logger:
         datefmt='%d.%m.%Y %H:%M:%S'
     )
 
-    filehandler = RotatingFileHandler(
-        filename='discord.log',
-        maxBytes=(5 * 1024 * 1024),
-        encoding='utf-8'
-    )
-    filehandler.setLevel(logging.INFO)
-    filehandler.setFormatter(logformat)
+    if config['LoggingFileHandler']:
+        filehandler = RotatingFileHandler(
+            filename='discord.log',
+            maxBytes=(config['LogFileSizeInMegabytes'] * 1024 * 1024),
+            encoding='utf-8'
+        )
+        filehandler.setLevel(logging.INFO)
+        filehandler.setFormatter(logformat)
+        logger.addHandler(filehandler)
 
-    streamhandler = logging.StreamHandler()
-    streamhandler.setLevel(logging.INFO)
-    streamhandler.setFormatter(logformat)
-
-    logger.addHandler(streamhandler)
-    logger.addHandler(filehandler)
+    if config['LoggingStreamHandler']:
+        streamhandler = logging.StreamHandler()
+        streamhandler.setLevel(logging.INFO)
+        streamhandler.setFormatter(logformat)
+        logger.addHandler(streamhandler)
 
     return logger
 
@@ -47,6 +48,7 @@ def get_intents() -> Intents:
     intents.voice_states = True
     return intents
 
+
 # Custom prefixes ----------------------------------------------------------- #
 def get_prefix(_, message) -> str:
     with open('prefixes.json', 'r') as f:
@@ -56,7 +58,10 @@ def get_prefix(_, message) -> str:
 
 # Main function ------------------------------------------------------------- #
 def main():
-    logger = setup_logger()
+    with open('config.json', 'r') as f:
+        config = json.load(f)
+
+    logger = setup_logger(config)
     load_dotenv()
 
     client = commands.Bot(command_prefix=get_prefix, intents=get_intents())
@@ -64,6 +69,9 @@ def main():
 
     for filename in listdir('./cogs'):
         if filename.endswith('.py'):
+            if filename[:-3] in config['BlacklistedCogs']:
+                logger.info(f'Skipped extension: {filename[:-3]}')
+                continue
             client.load_extension(f'cogs.{filename[:-3]}')
             logger.info(f'Loaded extension: {filename[:-3]}')
 
